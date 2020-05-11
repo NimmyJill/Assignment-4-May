640f86a05477255ca190b7d3b8d5bee93a990759
Nimmy Jill <nimmisarah@gmail.com>
2020-05-11 16:15:34 +0530

    Commit

--- script2.js
+++ script2.js
@@ -1,23 +1,21 @@
 function Qsn2(){
     
-    let Qn2="[ "+ document.getElementById("q2").value + " ]";//fetching the array
+   
+     let arr=[];
+     let size=5;
+
+     for (var i=0; i<size; i++){
+         arr[i]= prompt('Enter Element '+ (i+1));
+     }
     
-    let Q2= JSON.parse(Qn2);//return a JavaScript array, instead of a JavaScript object
-     
-
-
-
-    if(document.getElementById("q2").value==""){
-        document.getElementById("toDisplay").innerHTML="Field cannot be empty";
-        return false;
-    }
+    
     let element=0;
     let count=0;
-    for(let j=0; j<Q2.length;j++){
-        let tempElement=Q2[j];
+    for(let j=0; j<arr.length;j++){
+        let tempElement=arr[j];
         let tempCount=0;
-        for( let k=0; k<Q2.length; k++)
-            if(Q2[k]==tempElement)
+        for( let k=0; k<arr.length; k++)
+            if(arr[k]==tempElement)
             tempCount++;
             if(tempCount>count){
                 element= tempElement;
--- script8.js
+++ script8.js
@@ -1,11 +1,37 @@
 
 function Order(){
-let Arry8=document.getElementById("q8").value;
-let nonSortedArray= [...Arry8]
-var sortedArray = nonSortedArray.sort(function (a, b) {
-    if (a < b) return -1;
-    else if (a > b) return 1;
+
+
+
+  let array=[];
+  let size=3;
+  var out="";
+  var inp="";
+
+  for (var i=0; i<size; i++){
+      name= prompt("Enter name");
+      age= prompt("Enter age");
+      array.push({name:name,age:age});
+  }
+  for(let key in array){
+    console.log(array[key]);
+  }
+if (array!= null) {
+  for (let key in array){
+    inp+="Name: " + array[key].name + "         Age: " + array[key].age + "<br>"; 
+  }
+    document.getElementById("q8").innerHTML = inp;
+}
+
+var sortedArray = array.sort(function (a, b) {
+    var n1=a.name, n2=b.name
+    if (n1 < n2) return -1;
+    else if (n1 > n2) return 1;
     return 0;
   });
-document.getElementById("toDisplay").innerHTML=sortedArray;
+
+  for (let key in array){
+    out+="Name: " + array[key].name + "         Age: " + array[key].age + "<br>"; 
+  }
+document.getElementById("result").innerHTML=out;
 }
